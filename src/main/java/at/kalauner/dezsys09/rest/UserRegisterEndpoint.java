12a1035280b3960ec3b7e8d1c6b64d65de88e350
Paul Kalauner <pkalauner@student.tgm.ac.at>
2016-02-12 10:51:10 +0100

    users are now unique

--- src/main/java/at/kalauner/dezsys09/rest/UserRegisterEndpoint.java
+++ src/main/java/at/kalauner/dezsys09/rest/UserRegisterEndpoint.java
@@ -2,6 +2,7 @@ package at.kalauner.dezsys09.rest;
 
 import at.kalauner.dezsys09.db.User;
 import at.kalauner.dezsys09.db.UserRepository;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.transaction.TransactionSystemException;
 
 import javax.inject.Inject;
@@ -22,9 +23,14 @@ public class UserRegisterEndpoint {
     @POST
     public Response post(User user) {
         try {
-            User savedUser = this.userRepository.save(user);
-            return Response.status(Response.Status.CREATED).entity("User " + savedUser.getEmail() + " saved!").build();
-        } catch (TransactionSystemException cve) {
+            User fromDb = this.userRepository.findOne(user.getEmail());
+            if (fromDb == null) {
+                User savedUser = this.userRepository.save(user);
+                return Response.status(Response.Status.CREATED).entity("User " + savedUser.getEmail() + " saved!").build();
+            } else {
+                return Response.status(Response.Status.BAD_REQUEST).entity("User " + fromDb.getEmail() + " already exists!").build();
+            }
+        } catch (TransactionSystemException | InvalidDataAccessApiUsageException ex) {
             return Response.status(Response.Status.BAD_REQUEST).entity("Missing parameters!").build();
         }
     }
