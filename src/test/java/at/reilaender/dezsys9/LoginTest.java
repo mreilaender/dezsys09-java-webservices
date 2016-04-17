ad9f0ec53747116032146fd0d8018b28b15844f6
BuildTools <mreilaender@student.tgm.ac.at>
2016-04-17 13:12:25 +0200

    add wrong email, password, null email, null password, and null name

--- src/test/java/at/reilaender/dezsys9/LoginTest.java
+++ src/test/java/at/reilaender/dezsys9/LoginTest.java
@@ -1,10 +1,16 @@
 package at.reilaender.dezsys9;
 
+import at.reilaender.dezsys9.db.User;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.File;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author mreilaender
@@ -30,5 +36,62 @@ public class LoginTest {
             Application.main(new String[]{"--spring.profiles.active=test", "--server.port=" + port});
         else
             System.out.println(String.format("Could not delete or find embedded database file %s", embeddedDatabaseFilename));
+    }
+
+    @Test
+    public void testUserWrongEmail() {
+        User user = new User("", "user1", "secret");
+        try {
+            this.restTemplate.postForEntity(url + "/login", user, String.class);
+        } catch (HttpClientErrorException e) {
+            assertEquals("Invalid account data!", e.getResponseBodyAsString());
+            assertEquals(HttpStatus.FORBIDDEN, e.getStatusCode());
+        }
+    }
+
+    @Test
+    public void testUserWrongPassword() {
+        User user = new User("", "user1", "secret");
+        this.restTemplate.postForEntity(url + "/register", user, String.class);
+        user.setPassword("other_secret");
+        try {
+            this.restTemplate.postForEntity(url + "/login", user, String.class);
+        } catch (HttpClientErrorException e) {
+            assertEquals("Invalid account data!", e.getResponseBodyAsString());
+            assertEquals(HttpStatus.FORBIDDEN, e.getStatusCode());
+        }
+    }
+
+    @Test
+    public void testUserNullEmail() {
+        User user = new User(null, "user1", "secret");
+        try {
+            this.restTemplate.postForEntity(url + "/login", user, String.class);
+        } catch (HttpClientErrorException e) {
+            assertEquals("No email specified!", e.getResponseBodyAsString());
+            assertEquals(HttpStatus.FORBIDDEN, e.getStatusCode());
+        }
+    }
+
+    @Test
+    public void testUserWithNullPassword() {
+        User user = new User("user1@test.at", "user1", null);
+        try {
+            this.restTemplate.postForEntity(url + "/login", user, String.class);
+        } catch (HttpClientErrorException e) {
+            assertEquals("Invalid account data!", e.getResponseBodyAsString());
+            assertEquals(HttpStatus.FORBIDDEN, e.getStatusCode());
+        }
+    }
+
+    @Test
+    public void testUserWithNullName() {
+        User user = new User("user1@test.at", null, "secret");
+        try {
+            this.restTemplate.postForEntity(url + "/login", user, String.class);
+        } catch (HttpClientErrorException e) {
+            assertEquals("Invalid account data!", e.getResponseBodyAsString());
+            assertEquals(HttpStatus.FORBIDDEN, e.getStatusCode());
+        }
     }
 }
